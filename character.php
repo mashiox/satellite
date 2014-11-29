6b7cda08c9ec82cd7a952345fd0e28964eaaae27
Matt Walther <matt@mashio.net>
2014-11-28 17:02:51 -0800

    Style!

--- character.php
+++ character.php
@@ -1,12 +1,12 @@
 <?php
-header('Content-type: text/html; charset=utf-8');
-include_once 'config.php';
-include_once 'bnet.php';
-if (isset($_GET['name'], $_GET['region'], $_GET['realm'], $_GET['locale'], $_GET['fields'])){
-    echo BattleNet_getCharacter($_GET['name'], $_GET['realm'], $_GET['region'], $_GET['locale'], $_GET['fields']);
-}
-else {
-    echo json_encode(array("error"=>-1,'message'=>"Supply required parameters"));
-}
+    header('Content-type: text/html; charset=utf-8');
+    include_once 'config.php';
+    include_once 'bnet.php';
+    if (isset($_GET['name'], $_GET['region'], $_GET['realm'], $_GET['locale'], $_GET['fields'])){
+        echo BattleNet_getCharacter($_GET['name'], $_GET['realm'], $_GET['region'], $_GET['locale'], $_GET['fields']);
+    }
+    else {
+        echo json_encode(array("error"=>-1,'message'=>"Supply required parameters"));
+    }
 ?>
  
--- realm.php
+++ realm.php
@@ -1,11 +1,11 @@
 <?php
-header('Content-type: text/html; charset=utf-8');
-include_once 'config.php';
-include_once 'bnet.php';
-if (isset($_GET['region'], $_GET['locale'])){
-    echo BattleNet_getRealmStatus($_GET['region'], $_GET['locale']);
-}
-else {
-    echo json_encode(array("error"=>-1,'message'=>"Supply required parameters"));
-}
+    header('Content-type: text/html; charset=utf-8');
+    include_once 'config.php';
+    include_once 'bnet.php';
+    if (isset($_GET['region'], $_GET['locale'])){
+        echo BattleNet_getRealmStatus($_GET['region'], $_GET['locale']);
+    }
+    else {
+        echo json_encode(array("error"=>-1,'message'=>"Supply required parameters"));
+    }
 ?>
