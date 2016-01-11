ea5d348b4b4572ac66503e3f1c6c661acfd86ee9
Matt Walther <matt@mashio.net>
2016-01-10 18:23:47 -0800

    Comment clean up

--- server/bnet.php
+++ server/bnet.php
@@ -32,7 +32,6 @@ function BattleNet_getRealmStatus($region, $locale) {
         return FALSE;
     }
     $path = "/wow/realm/status";
-    //$url = "https://".strtolower($region).".api.battle.net".$path."?locale=".$locale."&apikey=".API_KEY;
     $url = bnetHelper::getBNetURL($region, $locale, $path);
     $header = bnetHelper::setHeader();
     $curl = curl_init();
@@ -54,14 +53,10 @@ function BattleNet_getRawItem($itemID, $region, $locale) {
     if (empty($itemID) || !is_numeric($itemID)){
         return FALSE;
     }
-    //$badCharacters = array('%21', '%26', '%27', '%28', '%29', '%3A', '%40', '+');
-    //$goodCharacters = array('!', '&', '\'', '(', ')', ':', '@', '-');
     $signPath = "/wow/item/".urlencode($itemID);
-    //$signPath = str_replace($badCharacters, $goodCharacters, $signPath);
     $signPath = str_replace(bnetHelper::badCharacters(), bnetHelper::goodCharacters(), $signPath);
 
     $path = "/wow/item/".$itemID;
-    //$url = "https://".strtolower($region).".api.battle.net".$path."?locale=".$locale."&apikey=".API_KEY;
     $url = bnetHelper::getBNetURL($region, $locale, $path);
     
     $header = bnetHelper::setHeader();
@@ -84,14 +79,10 @@ function BattleNet_getRawItemWithContext($itemID, $region, $locale, $context) {
     if (empty($itemID) || !is_numeric($itemID)){
         return FALSE;
     }
-    //$badCharacters = array('%21', '%26', '%27', '%28', '%29', '%3A', '%40', '+');
-    //$goodCharacters = array('!', '&', '\'', '(', ')', ':', '@', '-');
     $signPath = "/wow/item/".urlencode($itemID)."/".$context;
-    //$signPath = str_replace($badCharacters, $goodCharacters, $signPath);
     $signPath = str_replace(bnetHelper::badCharacters(), bnetHelper::goodCharacters(), $signPath);
 
     $path = "/wow/item/".$itemID."/".$context;
-    //$url = "https://".strtolower($region).".api.battle.net".$path."?locale=".$locale."&apikey=".API_KEY;
     $url = bnetHelper::getBNetURL($region, $locale, $path);
     
     $header = bnetHelper::setHeader();
